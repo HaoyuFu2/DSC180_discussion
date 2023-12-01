5b59d6b69cf0ba63eb802fcc660dfa84231c8db1
HaoyuFu2 <97488392+HaoyuFu2@users.noreply.github.com>
2023-12-01 10:54:49 -0800

    Updating helper functions

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from langdetect import detect
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.subplots_adjust(wspace=0.5)
 
 def sample_user(df, n=1):
     """
@@ -50,3 +53,12 @@ def sample_tweet_group(df, users, n=20):
         tweets = pd.DataFrame(tweets).assign(user_id=u)
         result = pd.concat([result, tweets])
     return result
+
+def visualize(df, question_No, topics):
+    col = 'score_topic_' + str(question_No)
+    fig, ax = plt.subplots(1, 2, sharex=False, sharey=True)
+    fig.suptitle('Distribution of agreement on ' + topics[question_No])
+    sns.histplot(ax=ax[0], data=df[df['candidate']=='trump'], x=col, stat='percent',
+                 hue='candidate', palette=['red'], discrete=True, binrange=[1,5])
+    sns.histplot(ax=ax[1], data=df[df['candidate']=='biden'], x=col, stat='percent',
+                 hue='candidate', palette=['blue'], discrete=True, binrange=[1,5])
--- in_context.py
+++ in_context.py
@@ -1,6 +1,8 @@
 import openai
+import pandas as pd
+import re
 
-openai.api_key='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'
+openai.api_key='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'
 
 def chat_with_model(prompt):
     completion = openai.ChatCompletion.create(
@@ -13,26 +15,78 @@ def chat_with_model(prompt):
     )
     return completion.choices[0].message["content"]
 
-def prompting(tweets, topic="improving LGBTQIA+ rights", candidate=True):
+def prompting(tweets, topics):
     tweets_text = "\n".join([f"Your tweet: ### {i} ###" for i in tweets])
-    # Question about candidates
-    if candidate:
-        question = f"""
-Who do you think is doing better on {topic}, Trump or Biden?
-State the candidate name without any other words in the same line.
-In a new line, explain the reasons behind your answer.
+
+    # Questions list
+    questions = ["""Question 1: 
+Do you support Donald Trump or Joe Biden in the upcoming 2020 Presidential Election?
+Your answer should be the last name of a candidate without any other words.
 """
-    # Question with topics
-    else:
-        question = f"""Please share your level of agreement with {topic}, with a score from 1 to 5, where 1 indicating strongly disagree and 5 indicating strongly agree.
+                 ]
+    
+    # Other questions
+    for i in range(len(topics)):
+        q = f"""Question {i+2}: 
+Please share your level of agreement with {topics[i]}, with a score from 1 to 5, where 1 indicating strongly disagree and 5 indicating strongly agree.
 The score should be an integer without any other words in the same line.
 In a new line, explain the reasons behind your level of agreement.
 """
+        questions.append(q)
+    questions = "\n\n".join(questions)
+    
     # Prompt
-    prompt = f'''Here are your tweets, you should try to find patteren in them and be ready to answer other questions with same attitude.
+    prompt = f'''Here are your tweets. You should try to find patterns in them and be ready to answer other questions with same attitude.
 {tweets_text}
 
-Your question is:
-{question}
+Please deliminate your answers to each question with a new line.
+Please don't include extra deliminaters, question numbers, etc.
+
+Your questions are:
+{questions}
 '''
     return prompt
+
+def generate_results(df, topics):
+    results = {}
+    for u in df['user_id'].unique():
+        tweets = df[df['user_id']==u]['cleaned_tweets']
+        prompt = prompting(tweets, topics)
+        result = chat_with_model(prompt)
+        results[u]=result
+    return results
+
+def parse_results(df, results, num_topics):
+    # df.groupby('user_id').agg({'cleaned_tweets':prompting}).reset_index().iloc[0]['cleaned_tweets']
+    
+    # Initializing returning dataframe
+    cols = ['user_id', 'candidate', 'hashtag']
+    for i in range(num_topics):
+        cols.append(f'score_topic_{i}')
+    df_result = pd.DataFrame({col:[] for col in cols})
+
+    # Parse responses for each user
+    for u, r in results.items():
+        ans_lst = r.split('\n')
+        while(ans_lst.count('')): 
+            ans_lst.remove('') 
+
+        # unify the format of candidate name
+        candidate = ans_lst[0]
+        if candidate.lower() in ['donald', 'donald trump', 'trump']:
+            candidate = 'trump'
+        elif candidate.lower() in ['joe', 'joe biden', 'biden']:
+            candidate = 'biden'
+        else:
+            candidate = 'N/A'
+
+        # parse scores
+        scores = re.findall(r'\n([0-9])\n', r)
+        ans_dict = {'user_id': u, 'candidate': candidate, 
+                    'hashtag':df[df['user_id']==u]['hashtag'].iloc[0]}
+        if len(scores) > 10: scores = scores[-10:]
+        for i in range(len(scores)):
+            ans_dict[cols[i+3]] = scores[i]
+        df_result = df_result.append(ans_dict, ignore_index=True)
+
+    return df_result
